810df6dc2379b4bda8009b81ce3cac79ee976344
fattechboy0226 <fatboy010226@gmail.com>
2025-05-10 03:06:21 -0700

    updated apis

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -113,6 +113,7 @@ export const getDiscussionDetails = async (req, res) => {
     const discussionPosts = await prisma.post.findMany({
       where: {
         postType: "discussion",
+        pId: { not: 0 }
       },
       orderBy: {
         createdAt: "desc", // Sort by createdAt descending
